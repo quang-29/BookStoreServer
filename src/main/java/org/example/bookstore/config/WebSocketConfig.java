21b58791b72be41132627b12005ea09f089f11ca
quang-29 <vquangn2990@gmail.com>
2025-04-14 13:11:34 +0700

    add notifications

---
+++ src/main/java/org/example/bookstore/config/WebSocketConfig.java
@@ -0,0 +1,90 @@
+package org.example.bookstore.config;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.units.qual.A;
+import org.example.bookstore.security.JWTUtils;
+import org.example.bookstore.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.converter.DefaultContentTypeResolver;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
+
+@Slf4j
+@Configuration
+@EnableWebSocketMessageBroker
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/user");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*");
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+        DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
+        resolver.setDefaultMimeType(MimeTypeUtils.APPLICATION_JSON);
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setObjectMapper(new ObjectMapper());
+        converter.setContentTypeResolver(resolver);
+        messageConverters.add(converter);
+        return true;
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor =
+                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                if(accessor != null && StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    String token = accessor.getFirstNativeHeader("Authorization");
+                    if(token != null && token.startsWith("Bearer ")) {
+                        token = token.substring(7);
+                        if(jwtTokenProvider.validateToken(token)){
+                            UsernamePasswordAuthenticationToken authToken = jwtTokenProvider.getAuthentication(token);
+                            SecurityContextHolder.getContext().setAuthentication(authToken);
+                            accessor.setUser(authToken);
+                            log.info("Websocket authenticated");
+                        }
+                    }
+                }
+                return message;
+            }
+        });
+    }
+}
---
+++ src/main/java/org/example/bookstore/payload/payment/PaymentDTO.java
@@ -0,0 +1,20 @@
+package org.example.bookstore.payload.payment;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.example.bookstore.enums.PaymentGateway;
+import org.example.bookstore.enums.PaymentStatus;
+import org.example.bookstore.enums.PaymentType;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class PaymentDTO {
+
+    private long amount;
+    private PaymentGateway gateway;
+    private PaymentType type;
+    private PaymentStatus status;
+    private Date createdAt = new Date();
+}
